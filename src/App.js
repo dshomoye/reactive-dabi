c132ed7f940ff57bc20ff709f8d4f8119f499438
dshomoye <dshomoye@gmail.com>
2017-08-04 13:29:46 -0400

    add material ui to proj

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <MuiThemeProvider>
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -13,7 +16,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <RaisedButton label = "Material UI!!" />
       </div>
+      </MuiThemeProvider>
     );
   }
 }
